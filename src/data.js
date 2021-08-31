d73346bfc72046e88da05b8d941809007369be58
Aman Shrivastava <aman34503@gmail.com>
2021-08-31 21:25:04 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const projects = [
+  
+  {
+    title: "Telegram Bot",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://github.com/aman34503",
+  },
+  {
+    title: "WebFlix",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://github.com/aman34503",
+  },
+  {
+    title: "Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://github.com/aman34503",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Shashank ",
+    company: "Tcs",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Akash",
+    company: "Persistant",
+  },
+];
+
+export const skills = [
+  "C++",
+  "JavaScript",
+  "React",
+  "Css",
+  "Node",
+  "Python",
+  "Sql",
+  "MongoDB",
+];
